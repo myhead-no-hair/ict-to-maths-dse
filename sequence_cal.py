c0e797d23d4b1a5d5f5aebb79715f41d705193cb
TreeTree Chess <jokerchess2005@gmail.com>
2026-05-15 03:51:10 +0800

    Update sequence_cal.py
    Add Geometry Sequence Calculator

--- sequence_cal.py
+++ sequence_cal.py
@@ -1,39 +1,57 @@
 import math
+from fractions import Fraction
 
 def fibonacci_seq(Term):
-    a = (1/2)*(1 + math.sqrt(5))
-    return math.trunc(math.sqrt(1/5)*(a**(Term) - (-a)**(-Term)))
+    a = (1/2)*(1 + (5)**(1/2))
+    return math.trunc(((1/5)**(1/2))
+                      *(a**(Term) - (-a)**(-Term)))
+    
+def produce_fraction_str(number):
+    if number.is_integer():
+        return str(number)
+    else:
+        return str(Fraction(number).limit_denominator().numerator)+"/"+str(Fraction(number).limit_denominator().denominator)
 
 while True:
-    Mode = input("Type of sequence(A = Arithmetic Sequence,F = Fibonacci Sequence,J = General Sequence):")
+    Mode = input("Type of sequence(A = Arithmetic Sequence, F = Fibonacci Sequence, D = Adding-Dots Sequence, G = Geometry Sequence):")
 
     if Mode == "A":
-        # Arithmetic Sequence
+        # General term of Arithmetic Sequence
+        print("="*8,"Arithmetic Sequence","="*8)
         while True:
-            Term_of_Na = int(input("Na th term of sequence:"))
-            Na = int(input("Integer of Na th term:"))
-            Term_of_Nb = int(input("Nb th term of sequence:"))
-            Nb = int(input("Integer of Nb th term:"))
-            if Term_of_Na == Term_of_Nb or Na == Nb:
-                print("Terms of sequence and integers of term cannot be the same. Please Re-enter.")
+            Term_of_Na = int(input("A th Term of sequence:"))
+            Na = float(input("Number of A th Term of sequence:"))
+            Term_of_Nb = int(input("B th Term of sequence:"))
+            Nb = float(input("Number of B th Term of sequence:"))
+            if Term_of_Na == Term_of_Nb:
+                print("Terms of sequence cannot be the same. Please Re-enter.")
+                continue
+            elif Na == Nb:
+                print("Numbers of Term of sequence cannot be the same. Please Re-enter.")
                 continue
             else:
                 break
-        Find_Term = int(input("Input Finding Term:"))
         Common_Difference = (Na - Nb)/(Term_of_Na - Term_of_Nb)
         NOne = Na - (Common_Difference * (Term_of_Na-1))
+        print("Common Difference of this Arithmetic Sequence:",produce_fraction_str(Common_Difference))
+        print("First Term of this Arithmetic Sequence:",produce_fraction_str(NOne))
+        Find_Term = int(input("Input Finding Term:"))
         Result = NOne + (Common_Difference * (Find_Term-1))
-        print("Integer of "+str(Find_Term)+" th term is :",str(int(Result)))
-    
+        print("Integer of "+str(Find_Term)+" th term:",produce_fraction_str(Result))
+        
     elif Mode == "F":
         # Fibonacci Sequence
+        print("="*8,"Fibonacci Sequence","="*8)
         while True:
-            Term_of_Na = int(input("Na th term of sequence:"))
-            Na = int(input("Integer of Na th term:"))
-            Term_of_Nb = int(input("Nb th term of sequence:"))
-            Nb = int(input("Integer of Nb th term:"))
-            if Term_of_Na == Term_of_Nb or Na == Nb:
-                print("Terms of sequence and integers of term cannot be the same. Please Re-enter.")
+            Term_of_Na = int(input("A th Term of sequence:"))
+            Na = float(input("Number of A th Term of sequence:"))
+            Term_of_Nb = int(input("B th Term of sequence:"))
+            Nb = float(input("Number of B th Term of sequence:"))
+            if Term_of_Na == Term_of_Nb:
+                print("Terms of sequence cannot be the same. Please Re-enter.")
+                continue
+            elif Na == Nb:
+                print("Numbers of Term of sequence cannot be the same. Please Re-enter.")
                 continue
             else:
                 break
@@ -50,12 +68,13 @@ while True:
             Result = NTwo
         else:
             Result = fibonacci_seq(Find_Term - 2) * NOne + fibonacci_seq(Find_Term - 1) * NTwo
-        print("Integer of "+str(Find_Term)+" th term is :",str(int(Result)))
+        print("Integer of "+str(Find_Term)+" th term:",str(int(Result)))
         
-    if Mode == "J":
-        # General Sequence
-        Term_of_Na = int(input("Na th term of sequence:"))
-        Na = int(input("Integer of Na th term:"))
+    elif Mode == "D":
+        # Adding-Dots Sequence
+        print("="*8,"Adding-Dots Sequence","="*8)
+        Term_of_Na = int(input("A th Term of sequence:"))
+        Na = float(input("Number of A th Term of sequence:"))
         Integeral_n = int(input("Integeral part of n of adding dots:"))
         Constant = int(input("Constant part of adding dots:"))
         Find_Term = int(input("Input Finding Term:"))
@@ -68,9 +87,41 @@ while True:
                 Result -= ((Integeral_n*n) + Constant)
         print("Integer of "+str(Find_Term)+" th term is :",str(int(Result)))    
         
+    elif Mode == "G":
+        # General term of Geometry Sequence
+        print("="*8,"Geometry Sequence","="*8)
+        while True:
+            Term_of_Na = int(input("A th Term of sequence:"))
+            Na = float(input("Number of A th Term of sequence:"))
+            Term_of_Nb = int(input("B th Term of sequence:"))
+            Nb = float(input("Number of B th Term of sequence:"))
+            if Term_of_Na == Term_of_Nb:
+                print("Terms of sequence cannot be the same. Please Re-enter.")
+                continue
+            elif Na == Nb:
+                print("Numbers of Term of sequence cannot be the same. Please Re-enter.")
+                continue
+            else:
+                break
+        Expo_Na = Term_of_Na - 1
+        # Expo_Nb = Term_of_Nb - 1
+        Expo_of_Common_Ratio = (Expo_Na - (Term_of_Nb - 1))# can be negative
+        Common_Ratio = (Na / Nb)**(1/Expo_of_Common_Ratio)
+        NOne = Na*(Common_Ratio ** (-Expo_Na))
+        if Expo_of_Common_Ratio//2 == 0:          
+            print("Common Ratio of this Geometry Sequence:",produce_fraction_str(Common_Ratio),"or",produce_fraction_str(Common_Ratio*-1))
+            if Expo_Na//2 == 0:# both even exponents
+                print("First Term of this Geometry Sequence:",produce_fraction_str(NOne))
+            else:# both odd exponents
+                print("First Term of this Geometry Sequence respect to the two Common Ratio:",produce_fraction_str(NOne),"or",produce_fraction_str(NOne*-1))
+        else:
+            print("Common Ratio of this Geometry Sequence:",produce_fraction_str(Common_Ratio))
+            print("First Term of this Geometry Sequence:",produce_fraction_str(NOne))
+        Find_Term = int(input("Input Finding Term:"))
+        Result = NOne*(Common_Ratio ** (Find_Term-1))
+        print("Number of "+str(Find_Term)+" th term:",produce_fraction_str(Result))
+                
     command = input("Quit? (Input \"Q\" to Quit):")
     if command == "Q":
         break
        
-    # elif Mode == "G":
-    #     # Geometry Sequence
